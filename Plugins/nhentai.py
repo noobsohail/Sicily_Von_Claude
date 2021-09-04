854f54451254619a341a169d1bab6aa7f1550209
noobsohail <sikhoshikaay@gmail.com>
2021-09-04 17:50:26 +0530

    done

--- Plugins/nhentai.py
+++ Plugins/nhentai.py
@@ -1,6 +1,6 @@
 from telethon import events
 import Helper.formating_results as format
-from API.nhentaiapi import nhentaiapi as hentai
+from API.nhentaiapi import nhentaiapi as nh
 from config import bot, ANIME_NO_IMG, BOT_USERNAME
 
 class Nhentai():
@@ -17,7 +17,7 @@ class Nhentai():
             text = event.raw_text.split()
             text.pop(0)
             code = " ".join(text)
-            chapter = hentai.get_chapter_by_code(code)
+            chapter = nh.get_chapter_by_code(code)
             format.manga_chapter_html(f"{code}", chapter)
             await bot.send_message(
                 event.chat_id,
