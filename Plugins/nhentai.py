fee7621fd5f187875cedb1b181f4c05b0a552a04
ꜱōʜᴇʀᴜ ᴋāɴ (AKA) ꜱᴏʜᴀɪʟ ᴋʜᴀɴ <87354331+noobsohail@users.noreply.github.com>
2021-09-04 17:25:58 +0530

    Update nhentai.py

--- Plugins/nhentai.py
+++ Plugins/nhentai.py
@@ -11,7 +11,7 @@ class Nhentai():
             await bot.send_message(
                 event.chat_id,
                 'Command must be used like this\n/nh <hentai code\nexample: /nh 339989',
-                file='{ANIME_NO_IMG}'
+                file=f'{ANIME_NO_IMG}'
             )
         elif '/nh' in event.raw_text:
             text = event.raw_text.split()
